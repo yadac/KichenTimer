30deece273c9be2abf87fbc7d5ffb4ca7a16b8c2
yosuke adachi <doruji1982@gmail.com>
2018-03-17 15:46:08 +0900

    add10min func

---
+++ KichenTimer/MyViewController.cs
@@ -0,0 +1,55 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace KichenTimer
+{
+    public partial class MyViewController : UIViewController
+    {
+        public MyViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            var buttons = new List<UIButton>()
+            {
+                StartButton,
+                ClearButton,
+                Add10MinButton,
+                Add1MinButton,
+                Add10SecButton,
+                Add1SecButton,
+            };
+
+            foreach (var button in buttons)
+            {
+                SetButtonOutline(button);
+            }
+
+            Add10MinButton.TouchUpInside += Add10MinButtonTouchUpInside;
+
+        }
+
+        private void SetButtonOutline(UIButton btn)
+        {
+            btn.Layer.CornerRadius = 3;
+            btn.Layer.BorderColor = UIColor.LightGray.CGColor; // what's CGColor?
+            btn.Layer.BorderWidth = 1;
+        }
+
+        private void Add10MinButtonTouchUpInside(object sender, EventArgs e)
+        {
+            _remain = _remain.Add(TimeSpan.FromMinutes(10));
+            ShowRemain();
+        }
+
+        private void ShowRemain()
+        {
+            RemainLabel.Text = $"{_remain.TotalMinutes:f0}:{_remain.Seconds:d2}";
+        }
+
+        private TimeSpan _remain = new TimeSpan(0);
+    }
+}
---
+++ KichenTimer/MyViewController.designer.cs
@@ -0,0 +1,83 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace KichenTimer
+{
+    [Register ("MyViewController")]
+    partial class MyViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Add10MinButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Add10SecButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Add1MinButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Add1SecButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ClearButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel RemainLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton StartButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Add10MinButton != null) {
+                Add10MinButton.Dispose ();
+                Add10MinButton = null;
+            }
+
+            if (Add10SecButton != null) {
+                Add10SecButton.Dispose ();
+                Add10SecButton = null;
+            }
+
+            if (Add1MinButton != null) {
+                Add1MinButton.Dispose ();
+                Add1MinButton = null;
+            }
+
+            if (Add1SecButton != null) {
+                Add1SecButton.Dispose ();
+                Add1SecButton = null;
+            }
+
+            if (ClearButton != null) {
+                ClearButton.Dispose ();
+                ClearButton = null;
+            }
+
+            if (RemainLabel != null) {
+                RemainLabel.Dispose ();
+                RemainLabel = null;
+            }
+
+            if (StartButton != null) {
+                StartButton.Dispose ();
+                StartButton = null;
+            }
+        }
+    }
+}
